c7466a6a7561f60917d414f814e944240f645bed
PhilippMaschi <phil.masch@hotmail.com>
2023-07-20 16:13:38 +0200

    mosis_wonder.py: works!

--- mosis_wonder.py
+++ mosis_wonder.py
@@ -5,42 +5,62 @@ import geopandas as gpd
 from shapely.geometry import shape
 from shapely.ops import unary_union
 from shapely.strtree import STRtree
+from pathlib import Path
+from shapely.geometry import MultiLineString, LineString
+import itertools
 
 
-def calc_premeter(input_lyr, output_lyr, positive_buffer, negative_buffer):
+
+def calc_premeter(input_lyr, output_lyr, positive_buffer: float, negative_buffer: float):
     st = time.time()
     if positive_buffer + negative_buffer >= 0:
         print("Absolut value of the negative buffer should be greater than the positive buffer")
         return
-    orig_poly = [shape(feature['geometry']) for feature in fiona.open(input_lyr)]
+    orig_poly = []
+    for feature in fiona.open(input_lyr):
+        geometry = shape(feature['geometry'])
+        if geometry.geom_type == "Polygon" and geometry.is_valid:
+            orig_poly.append(geometry)
+
     # +positive_buffer meter buffer
-    orig_poly_buffer = [shape(feature['geometry']).buffer(positive_buffer) for feature in fiona.open(input_lyr)]
+    orig_poly_buffer = [geometry.buffer(positive_buffer) for geometry in orig_poly]
     # polygon union
     poly_buffer_union = unary_union(orig_poly_buffer)
     # negative_buffer meter buffer: overlays inside the original layer
-    boundary_buffer_union = [g.buffer(negative_buffer).boundary for g in poly_buffer_union]
+    boundary_buffer_union = [g.buffer(negative_buffer).boundary for g in poly_buffer_union.geoms]
+
+    # Combine the boundary buffers
     buffer_union = unary_union(boundary_buffer_union)
+    # Now, calculate the perimeter of the union of buffers (positive and negative combined)
     premeter = []
     # use Sort-Tile-Recursive for spatial query
-    tree = STRtree(buffer_union)
+    tree = STRtree(buffer_union.geoms)
+
     for i, item in enumerate(orig_poly):
-        buffer_union_query = unary_union(tree.query(item))
+        # query from the tree returns the indices from buffer_union
+        tree_query = tree.query(item)
+        # Get the actual intersecting geometries using the indices
+        intersecting_geometries = [buffer_union.geoms[index] for index in tree_query]
+        # create unary union from the intersecting geometries
+        buffer_union_query = unary_union(intersecting_geometries)
+        # create temporary segment of the intersections to save time
         temp_segment = item.intersection(buffer_union_query)
         accumulator = 0
         if temp_segment.geom_type == "MultiLineString":
-            accumulator += sum([g.length for g in temp_segment])
+            accumulator += sum([g.length for g in temp_segment.geoms])
         elif temp_segment.geom_type == "LineString":
             accumulator = temp_segment.length
         else:
             print('Check item: ', i)
         premeter.append(accumulator)
 
+
     gdf = gpd.read_file(input_lyr)
     gdf['exPremeter'] = np.array(premeter)
     gdf.to_file(output_lyr)
     gdf = None
     # calculate deviation
-    approx_extern_premeter = sum([elem.length for elem in buffer_union])
+    approx_extern_premeter = sum([elem.length for elem in buffer_union.geoms])
     returned_premeter = sum(premeter)
     approx_dev_percent = 100 * (approx_extern_premeter - returned_premeter) / approx_extern_premeter
     elapsed = time.time() - st
@@ -56,9 +76,9 @@ def calc_premeter(input_lyr, output_lyr, positive_buffer, negative_buffer):
 
 
 if __name__ == "__main__":
-    path = 'C:/Users/Mostafa/Desktop/QGIS_Shapes_ETRS/'
-    input_lyr = path + 'selection_3035.shp'
-    output_lyr = path + 'outside_premeter.shp'
+    path = Path(r'C:\Users\mascherbauer\PycharmProjects\OSM')
+    input_lyr = path / 'merged_osm_geom.shp'
+    output_lyr = path / 'new_merged_osm_geom.shp'
     positive_buffer = 0.1
     negative_buffer = -0.11
     calc_premeter(input_lyr, output_lyr, positive_buffer, negative_buffer)
