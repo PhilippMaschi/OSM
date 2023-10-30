875d40323f5e341841bd543677cd6c9ab9274f3d
PhilippMaschi <phil.masch@hotmail.com>
2023-10-30 13:02:54 +0100

    global_buildings.py: load data for the sites from global buildings works (with building height in some cases)

---
+++ global_buildings.py
@@ -0,0 +1,118 @@
+import osmnx as ox
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import box, shape
+import rasterio
+from main import LEEUWARDEN, MURCIA, KWIDZYN, BAARD, RUMIA, SUCINA
+from rasterio.features import shapes
+from osgeo import gdal
+import fiona
+from pathlib import Path
+
+
+def check_if_buildings_are_in_area(gdf: gpd.GeoDataFrame, city: dict):
+    # aoi_geom = {
+    #     "coordinates": [
+    #         [
+    #             [LEEUWARDEN["west"], LEEUWARDEN["north"]],
+    #             [LEEUWARDEN["west"], LEEUWARDEN["south"]],
+    #             [LEEUWARDEN["east"], LEEUWARDEN["north"]],
+    #             [LEEUWARDEN["east"], LEEUWARDEN["south"]],
+    #         ],
+    #     ],
+    #     "type": "Polygon",
+    #
+    # }
+    # aoi_shape = shape(aoi_geom)
+    # minx, miny, maxx, maxy = aoi_shape.bounds
+    bbox = box(city["west"], city["south"], city["east"], city["north"])
+    any_within = gdf["geometry"].within(bbox).any()
+    if any_within:
+        return True
+    else:
+        return False
+
+
+def load_buildings_from_global_buildings(country: str, city: dict):
+    """
+    This snippet demonstrates how to access and convert the buildings
+    data from .csv.gz to geojson for use in common GIS tools. You will
+    need to install pandas, geopandas, and shapely.
+
+    turns out that murcia is not in the dataset available
+
+    """
+    dataset_links = pd.read_csv("https://minedbuildings.blob.core.windows.net/global-buildings/dataset-links.csv")
+    links = dataset_links[dataset_links.Location == country]
+    for _, row in links.iterrows():
+        df = pd.read_json(row.Url, lines=True)
+        df['geometry'] = df['geometry'].apply(shape)
+        gdf = gpd.GeoDataFrame(df, crs=4326)
+        within = check_if_buildings_are_in_area(gdf, city)
+        print(f"{row.QuadKey}: {within}")
+        if within:
+            gdf_cutted = gdf.cx[city["west"]: city["east"], city["south"]: city["north"]].copy()
+            new_gdf = gdf_cutted.to_crs("epsg:3035")
+            new_gdf.to_file(Path("global_buildings") / f"{country}" / f"{city['city_name']}_{row.QuadKey}.geojson",
+                        driver="GeoJSON")
+
+
+def copernicus():
+    """
+    data from https://ghsl.jrc.ec.europa.eu/download.php?ds=builtH
+    :return:
+    """
+
+    # Path to the GeoTIFF file
+    tif_path = r'C:\Users\mascherbauer\PycharmProjects\OSM\copernicus_spain_heights.tif'
+
+    # Read the GeoTIFF file using rasterio
+    with rasterio.open(tif_path) as src:
+        # Read the raster data as a numpy array
+        raster_data = src.read(1)  # Assuming a single band GeoTIFF
+
+        # Retrieve the spatial information (e.g., CRS and transform)
+        crs = src.crs
+        transform = src.transform
+
+    # Create a list to store the polygon geometries
+    polygons = []
+
+    # Generate polygons from the raster data
+    i = 0
+    for geom, value in shapes(raster_data, transform=transform):
+        if value != 0:  # Skip empty polygons
+            polygons.append(shape(geom))
+            print(i)
+            i += 1
+
+    # Create a GeoDataFrame from the polygons
+    gdf = gpd.GeoDataFrame(geometry=polygons, crs=crs)
+
+    # Print the GeoDataFrame
+    print(gdf.head())
+    gdf.to_file(f"copernicus.geojson", driver="GeoJSON")
+
+
+def read_copernicus():
+    tif_path = r'C:\Users\mascherbauer\PycharmProjects\OSM\copernicus_spain_heights.tif'
+    ds = gdal.Open(tif_path)
+    b = ds.GetRasterBand(1)
+    arr = b.ReadAsArray()
+
+    # todo tiff coordinatensystem heraus finden
+    # todo zonal statistics um tiff mit murcia df zu "mergen" Rasterstats. Das heißt wir nehmen die Grundfläche von OSM oder URBAN3R
+    # um dann diese mit den rasterstats von https://ghsl.jrc.ec.europa.eu/download.php?ds=builtH zu mergen damit wir
+    # die Gebäudehöhe erhalten
+
+    # with  gdal.Open(tif_path) as gdal_df:
+
+    # murcia_gdf = gdf.contains()
+
+
+if __name__ == '__main__':
+    load_buildings_from_global_buildings("Netherlands", LEEUWARDEN)
+    load_buildings_from_global_buildings("Netherlands", BAARD)
+    load_buildings_from_global_buildings("Poland", KWIDZYN)
+    load_buildings_from_global_buildings("Poland", RUMIA)
+    # read_copernicus()
