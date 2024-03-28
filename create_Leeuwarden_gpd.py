2a749e10cde48667658960035c17e7c895ae76f9
PhilippMaschi <phil.masch@hotmail.com>
2024-03-28 14:55:43 +0100

    leuwwarden progress

--- create_Leeuwarden_gpd.py
+++ create_Leeuwarden_gpd.py
@@ -1,9 +1,10 @@
 import geopandas as gpd
 from pathlib import Path
 import pandas as pd
-from main import BASE_EPSG, LEEUWARDEN
+from main import BASE_EPSG, LEEUWARDEN, find_construction_period, get_parameters_from_dynamic_calc_data
 from tqdm import tqdm
 from load_invert_data import get_number_of_buildings_from_invert
+from mosis_wonder import calc_premeter
 import numpy as np
 
 columns_2_drop = [
@@ -39,7 +40,11 @@ columns_2_drop = [
     'tijdstipnietbaglv',
     'tijdstipregistratie',
     'tijdstipregistratielv',
-    'b3_h_maaiveld'
+    'b3_dd_id',
+    'b3_pand_deel_id',
+    'b3_h_70p',
+    'b3_bouwlagen',
+    'b3_kwaliteitsindicator',
 ]
 
 filter_dict = {
@@ -58,6 +63,11 @@ translation_dict = {
     'b3_volume_lod22': "building_volume",
     'oorspronkelijkbouwjaar': "original_year_of_construction",
     'voorkomenidentificatie': "occurrence_identification",
+    'b3_h_maaiveld': "ground_level",
+    "b3_h_dak_50p": "median_roof_height",
+    "b3_h_dak_min": "minimum_roof_height",
+    "b3_h_max": "maximum_roof_height",
+
 }
 
 osm_building_types_filter = {
@@ -110,6 +120,8 @@ osm_building_types_filter = {
 
 
 def merge_gpkg_files():
+    # AFTER finding the database on QGIS and cutting it from there this function is not needed anymore! produes
+    # gdf with missing data (building heights) were missing.
     """{
     "identificationInfo": {
     "citation": {
@@ -119,10 +131,13 @@ def merge_gpkg_files():
     "edition": "v2023.10.08",
     "identifier": "3652fa0c-6566-11ee-bc83-858db9e376fa"
 },"""
+    print("merging gpk files")
     path_2_files = Path(r"C:\Users\mascherbauer\PycharmProjects\OSM\input_data\TU_Delft")
     files = list(path_2_files.glob("*.gpkg"))
     big_df = gpd.GeoDataFrame()
     for i, file in tqdm(enumerate(files)):
+        if "lod2d" in file.stem.lower():
+            continue
         df = gpd.read_file(file)
         if i == 0:
             crs = df.crs
@@ -138,8 +153,10 @@ def merge_gpkg_files():
     new_df = big_df.to_crs(BASE_EPSG)
     leeuwarden_df = new_df.cx[LEEUWARDEN["west"]: LEEUWARDEN["east"], LEEUWARDEN["south"]: LEEUWARDEN["north"]]
     leeuwarden_df.to_file(Path(r"input_data/TU_Delft") / f"Leeuwarden.gpkg", driver="GPKG")
+    print("saved Leeuwarden gpkg file")
 
-def manual_removal_of_large_non_residential_buildings():
+
+def manual_removal_of_large_non_residential_buildings(dataframe: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
     ids_to_remove = [
         23693,
         26054,
@@ -179,27 +196,52 @@ def manual_removal_of_large_non_residential_buildings():
         3924,
         3897,
         3195,
+        18581,
+        17978,
+        17822,
+        18392,
+        18406,
+        17237,
+        22263,
+
     ]
+    # create set out of list in case buildigns were selected twice:
+    to_remove = set(ids_to_remove)
+    df_filtered = dataframe[~dataframe['OSM_ID'].isin(to_remove)]
+    print("manually removed industrial buildings from dataset")
+    return df_filtered
 
 
-def add_osm_information_to_leeuwarden(delft_df: gpd.GeoDataFrame):
+def add_osm_information_to_leeuwarden():
+    # load OSM
     osm_df = gpd.read_file(Path(r"input_data/OpenStreetMaps") / "Leeuwarden.gpkg")
     osm_df["help_geometry"] = osm_df["geometry"]
     osm_df["geometry"] = osm_df.representative_point()
     osm_df.drop(columns=["help_geometry"]).to_file(Path(r"input_data") / "Leeuwarden_OSM.gpkg", driver="GPKG")
-    merged = gpd.sjoin(delft_df, osm_df, how='inner', op='contains')
+    # load TU Delft data
+    lod12d = gpd.read_file(Path(r"input_data/TU_Delft") / "LoD12D.gpkg")
+    if lod12d.crs.to_epsg() != BASE_EPSG:
+        lod12d.to_crs(BASE_EPSG)
+    leeuwarden = lod12d.cx[LEEUWARDEN["west"]: LEEUWARDEN["east"], LEEUWARDEN["south"]: LEEUWARDEN["north"]].copy()
+    leeuwarden = leeuwarden.drop(columns=columns_2_drop)
+    trans = leeuwarden.rename(columns=translation_dict)
+    # merge dataframes and filter
+    merged = gpd.sjoin(trans, osm_df, how='inner', op='contains')
     columns_to_drop = merged.columns[
-        ~merged.columns.isin(delft_df.columns) & ~merged.columns.isin(['building', 'index_right', 'amenity'])]
+        ~merged.columns.isin(trans.columns) & ~merged.columns.isin(['building', 'index_right', 'amenity'])]
     result = merged.drop(columns=columns_to_drop, axis=1)
     mask = result["building"].map(osm_building_types_filter)
     filtered_df = result[mask]
     mask2 = filtered_df["amenity"].isna()
     filtered_2_df = filtered_df[mask2]
     # drop all buildings that have below 45m2 floor area
-    larger_buildings = filtered_2_df[filtered_2_df["area"] > 45]
+    larger_buildings = filtered_2_df[filtered_2_df["area"] > 45].reset_index(drop=True)
     # this index is used to kick buildings manually!
-    larger_buildings.rename(columns={"index_right": "OSM_ID"}, inplace=True)
-    larger_buildings.drop(columns=["amenity"]).to_file(Path(r"input_data") / "Leeuwarden.gpkg", driver="GPKG")
+    larger_buildings = larger_buildings.rename(columns={"index_right": "OSM_ID"})
+    # now kick manual selected buildings:
+    complete_filtered = manual_removal_of_large_non_residential_buildings(larger_buildings)
+
+    complete_filtered.drop(columns=["amenity"]).to_file(Path(r"input_data") / "Leeuwarden.gpkg", driver="GPKG")
 
 
 def add_location_point_to_gdf(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
@@ -211,9 +253,11 @@ def prepare_data_for_5R1C_transition():
     pass
 
 
-def load_invert_netherlands_data(invert_city_filter_name: str, country: str):
+def load_invert_netherlands_data(invert_city_filter_name: str, country: str, year: int, scen: str):
     df_invert = get_number_of_buildings_from_invert(invert_city_filter_name=invert_city_filter_name,
-                                                    country=country)
+                                                    country=country,
+                                                    year=year,
+                                                    scen=scen)
 
     df_invert.loc[:, "construction_period"] = df_invert.loc[:, "construction_period_start"].astype(str) + "-" + \
                                               df_invert.loc[:, "construction_period_end"].astype(str)
@@ -225,9 +269,6 @@ def load_invert_netherlands_data(invert_city_filter_name: str, country: str):
                 (df_invert.loc[:, "construction_period"] != "1975-1990") &
                 (df_invert.loc[:, "construction_period"] != "2000-2008"),
                 :]
-    # Removing b' ' prefix from name
-    df_invert['name'] = df_invert['name'].str.replace("b'", '', regex=False)
-    df_invert['name'] = df_invert['name'].str.replace("'", '', regex=False)
     # remove non residential buildings by filtering for MFH and SFH
     filtered = df_invert[df_invert['name'].str.contains('SFH|MFH')].reset_index(drop=True)
     # correct the construction periods to not leave out random years (eg. 2006 and 2007 is not covered by any period)
@@ -236,69 +277,114 @@ def load_invert_netherlands_data(invert_city_filter_name: str, country: str):
     return filtered
 
 
-def map_invert_data_to_buildings(df: gpd.GeoDataFrame, country: str, invert_city_filter_name: str):
-    df["original_year_of_construction"] = df["original_year_of_construction"].astype(int)
-    df_invert = load_invert_netherlands_data(invert_city_filter_name=invert_city_filter_name, country=country)
+def calculate_5R1C_necessary_parameters(df, year: int, country: str, scen: str):
+    # height of the building
+    df.loc[:, "height"] = df["b3_h_min"] - df["ground_level"]
+    # adjacent area
+    df.loc[:, "free wall area (m2)"] = df.loc[:, "free length (m)"] * df.loc[:, "height"]
+    # not adjacent area
+    df.loc[:, "adjacent area (m2)"] = (df.loc[:, "circumference (m)"] - df.loc[:, "free length (m)"]) * \
+                                      df.loc[:, "height"]
+    # total wall area
+    df.loc[:, "wall area (m2)"] = df.loc[:, "circumference (m)"] * df.loc[:, "height"]
+    # ration of adjacent to not adjacent (to compare it to invert later)
+    df.loc[:, "percentage attached surface area"] = df.loc[:, "adjacent area (m2)"] / df.loc[:, "wall area (m2)"]
+    df_return = get_parameters_from_dynamic_calc_data(df, year, country, scen)
+
+    # demographic information: number of persons per house is number of dwellings (numero_viv) from Urban3R times number
+    # of persons per dwelling from invert
+    df_return.loc[:, "person_num"] = df_return.loc[:, "numero_viv"] * df_return.loc[:, "number_of_persons_per_dwelling"]
+    # building type:
+    df_return.loc[:, "type"] = ["SFH" if i == 1 else "MFH" for i in df_return.loc[:, "numero_viv"]]
+    return df_return
+
+
+def map_invert_data_to_buildings(gdf: gpd.GeoDataFrame,
+                                 country: str,
+                                 invert_city_filter_name: str,
+                                 year: int,
+                                 scen: str):
+    gdf["original_year_of_construction"] = gdf["original_year_of_construction"].astype(int)
+    df_invert = load_invert_netherlands_data(invert_city_filter_name=invert_city_filter_name,
+                                             country=country,
+                                             year=year,
+                                             scen=scen)
 
     # group invert data after construction period:
     construction_periods = list(df_invert.loc[:, "construction_period"].unique())
 
-    # for each building in the gdf extract the type (SFH or MFH or something else) and the year of construction
-    # types = gdf["uso_princi"].unique()
-    # print(types)
-    # type_groups = gdf.groupby("uso_princi")
-    # complete_df = pd.DataFrame()
-    # for type, group in type_groups:
-    #     # add construction year to the buildings that don't have one
-    #     # group["ano_construccion"] = replace_nan_with_distribution(group, "ano_construccion")
-    #     group["ano_constr"] = group["ano_constr"].astype(int)
-    #     # add the number of stories of the building if not known
-    #     # group["altura_maxima"] = replace_nan_with_distribution(group, "altura_maxima")
-    #
-    #     # select a building type from invert based on the construction year and type
-    #     group["invert_construction_period"] = group["ano_constr"].apply(
-    #         lambda x: find_construction_period(construction_periods, x)
-    #     )
+    # for some reason, some buildings have a negative building volume. We will exclude them without further investigating
+    gdf = gdf.loc[gdf.loc[:, "building_volume"] > 0, :]
+
+    # in Leeuwarden we dont have the classification from the Delft data on the building type (SFH or MFH)
+    # therefore we choose this label based on the volume of the building. 450m3 was choosen because then the ration
+    # of SFH to MFH resembles aproximately the ration of open source data from: https://allcharts.info/the-netherlands/municipality-leeuwarden/
+    # Also the labels from Open Street Maps align well with this approach for the buildings where the labels are available
+    gdf.loc[:, "type"] = ""
+    gdf.loc[gdf.loc[:, "building_volume"] < 450, "type"] = "SFH"
+    gdf.loc[gdf.loc[:, "building_volume"] >= 450, "type"] = "MFH"
+
+    # set construction year to int:
+    gdf["original_year_of_construction"] = gdf["original_year_of_construction"].astype(int)
+
+    # we use the minimum building height because we neglect rooms in the attic for the purpose of simlplicity:
+    gdf_5r1c = calculate_5R1C_necessary_parameters(gdf, year=year, country=country, scen=scen)
+
+
+    type_groups = gdf.groupby("type")
+    df_list = []
+    np.random.seed(42)
+    for type, group in type_groups:
+        # add the number of stories of the building if not known
+        # group["altura_maxima"] = replace_nan_with_distribution(group, "altura_maxima")
+
+        # select a building type from invert based on the construction year and type
+        group["invert_construction_period"] = group["ano_constr"].apply(
+            lambda x: find_construction_period(construction_periods, x)
+        )
         # invert_selection = filter_invert_data_after_type(type=type, invert_df=df_invert)  # does not work for residential (i have a mistake)
 
         # now select a representative building from invert for each building from Urban3r:
-    #     for i, row in group.iterrows():
-    #         close_selection = df_invert.loc[
-    #                           df_invert["construction_period"] == row['invert_construction_period'], :
-    #                           ]
-    #         # distinguish between SFH and MFH
-    #         if row["tipologia_"] == "Unifamiliar":
-    #             sfh_or_mfh = "SFH"
-    #         else:
-    #             sfh_or_mfh = "MFH"
-    #         mask = close_selection["name"].isin([name for name in close_selection["name"] if sfh_or_mfh in name])
-    #         type_selection = close_selection.loc[mask, :]
-    #         if type_selection.shape[0] == 0:  # if only SFH or MFH available from invert take this data instead
-    #             type_selection = close_selection
-    #         # now get select the building after the probability based on the distribution of the number of buildings
-    #         # in invert:
-    #         distribution = type_selection["number_of_buildings"].astype(float) / \
-    #                        type_selection["number_of_buildings"].astype(float).sum()
-    #         # draw one sample from the distribution
-    #         random_draw = np.random.choice(distribution.index, size=1, p=distribution.values)[0]
-    #         selected_building = type_selection.loc[random_draw, :]
-    #         # delete number of buildings because this number is 1 for a single building
-    #         complete_df = pd.concat([
-    #             complete_df,
-    #             pd.DataFrame(pd.concat([selected_building, row.drop(columns=["number_of_buildings"])], axis=0)).T
-    #         ], axis=0
-    #         )
-    #
-    # final_df = complete_df.reset_index(drop=True)
-    # return final_df
+        for i, row in group.iterrows():
+            close_selection = df_invert.loc[
+                              df_invert["construction_period"] == row['invert_construction_period'], :
+                              ]
+            # distinguish between SFH and MFH
+            if row["tipologia_"] == "Unifamiliar":
+                sfh_or_mfh = "SFH"
+            else:
+                sfh_or_mfh = "MFH"
+            mask = close_selection["name"].isin([name for name in close_selection["name"] if sfh_or_mfh in name])
+            type_selection = close_selection.loc[mask, :]
+            if type_selection.shape[0] == 0:  # if only SFH or MFH available from invert take this data instead
+                type_selection = close_selection
+            # now get select the building after the probability based on the distribution of the number of buildings
+            # in invert:
+            distribution = type_selection["number_of_buildings"].astype(float) / \
+                           type_selection["number_of_buildings"].astype(float).sum()
+            # draw one sample from the distribution
+            random_draw = np.random.choice(distribution.index, size=1, p=distribution.values)[0]
+            selected_building = type_selection.loc[random_draw, :]
+            # delete number of buildings because this number is 1 for a single building
+            df_list.append(
+                pd.DataFrame(pd.concat([selected_building, row.drop(columns=["number_of_buildings"])], axis=0)).T)
+
+    final_df = pd.concat(df_list, axis=0).reset_index(drop=True)
 
 
 if __name__ == "__main__":
-    file_name = Path(r"input_data/TU_Delft") / f"Leeuwarden.gpkg"
-    if not file_name.exists():
-        merge_gpkg_files()
-    df = gpd.read_file(file_name)
-    add_osm_information_to_leeuwarden(delft_df=df)
+    Year = 2020
+    scenario = "high_eff"
+    combined_file_name = Path(r"input_data") / "Leeuwarden.gpkg"
+    extended_file_name = Path(r"input_data") / "Leeuwarden_extended.gpkg"
+    if not extended_file_name.exists():
+        add_osm_information_to_leeuwarden()
+        # add the adjacent length and circumference with mosis wonder:
+        big_df = calc_premeter(input_lyr=combined_file_name,
+                               output_lyr=extended_file_name)
 
-    df_loc = add_location_point_to_gdf(df)
-    map_invert_data_to_buildings(df_loc, invert_city_filter_name="", country="Netherlands")
+
+    combined_file = gpd.read_file(extended_file_name)
+    df_loc = add_location_point_to_gdf(combined_file)
+
+    map_invert_data_to_buildings(df_loc, invert_city_filter_name="", country="Netherlands", year=Year, scen=scenario)
